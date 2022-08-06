078c2ba5b41887c136225f0854fd4680688174b9
facuariasla <facundolautaroarias@hotmail.com>
2022-08-06 10:02:40 -0300

    data sended to API, and getting response - first commit

---
+++ pages/login.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useState } from "react";
+
+const LoginPage = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    console.log(credentials)
+    const response = await axios.post('/api/auth/login', credentials)
+    console.log(response);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="email"
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
